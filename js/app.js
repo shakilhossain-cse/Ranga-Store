35117be77ef746f6090ab18f8c7442d4c6dded52
Shakil Hossain <shakilhossainp.cse@gmail.com>
2021-09-13 14:13:21 +0600

    add rating and total rating

--- js/app.js
+++ js/app.js
@@ -4,11 +4,12 @@ const loadProducts = () => {
 };
 
 
+
 // show all product in UI 
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    // console.log(product);
+    // console.log(product.rating.rate.round());
     const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
@@ -18,6 +19,14 @@ const showProducts = (products) => {
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
+      <p class="rating">
+      <span class="fa fa-star checked"></span>
+      ${product.rating.rate}
+      </p>
+      <h4 class="rating">
+      Total Review  (${product.rating.count})
+      
+      </h4>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
@@ -25,6 +34,10 @@ const showProducts = (products) => {
     document.getElementById("all-products").appendChild(div);
   }
 };
+
+
+
+// Add to Cart  
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
